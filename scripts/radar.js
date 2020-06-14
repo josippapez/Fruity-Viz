0e0bcf5b9700b849839e1f75822ebb9e39a29164
Josip Papez <papezjosip@gmail.com>
2020-06-14 11:19:28 +0200

    styling fixes, added radar chart

--- scripts/radar.js
+++ scripts/radar.js
@@ -1,66 +1,63 @@
-// Funkcija za crtanje stupčastog grafa, ispis imena Pokemona, prikaz slike Pokemona i crtanje radarskog grafa
-// Parametri: skup podataka o Pokemonima, ID Pokemona koji se vizualizira, te pokemonNum (označava poziciju na
+// Funkcija za crtanje radarskog grafa
+// Parametri: skup podataka o voću, ID voća koji se vizualizira, te fruitNum (označava poziciju na
 // stranici, ako je lijevo, onda je 1, ako je desno onda je 2)
-function drawAll(data, id, pokemonNum) {
-    // margine, visina i širina radara
-    var marginRadar = {
-        top: 100,
-        right: 100,
-        bottom: 100,
-        left: 100
+function drawAll(data, id, fruitNum, maxValue) {
+  // margine, visina i širina radara
+  var marginRadar = {
+      top: 100,
+      right: 100,
+      bottom: 100,
+      left: 100,
     },
-        widthRadar = Math.min(700, window.innerWidth - 10) - marginRadar.left - marginRadar.right,
-        heightRadar = Math.min(widthRadar, window.innerHeight - marginRadar.top - marginRadar.bottom - 20);
+    widthRadar = 500,
+    heightRadar = 500;
 
-    // Prikaz imena i slike pokemona
-    d3.select(`#pokemon${pokemonNum}name`).html(data[id - 1]["Name"])
-    d3.select(`#pokemon${pokemonNum}Image`).attr("src", `https://www.serebii.net/art/th/${id}.png`)
-    // Inicijalizacija polja podataka za Pokemona koji se vizualizira
-    pokeData[pokemonNum - 1] = []
 
-    // Ako se prvo odabire crtanje Pokemona na desnoj strani (drugi Pokemon), podaci
-    // za prvog Pokemona se postavljaju na 0 zbog načina na koji je biblioteka napisana
-    if (pokemonNum == 2 && pokeData[0].length == 0) {
-        for (key in allowedKeys) {
-            pokeData[0].push({
-                axis: allowedKeys[key],
-                value: 0
-            })
-        }
-    }
-    // Formatiranje podataka kako bi se mogla koristiti RadarChart biblioteka
+
+  // Inicijalizacija polja podataka za voće koji se vizualizira
+  fruitData[fruitNum - 1] = [];
+
+  // Ako se prvo odabire crtanje voća na desnoj strani (drugo voće), podaci
+  // za prvog voća se postavljaju na 0 zbog načina na koji je biblioteka napisana
+  if (fruitNum == 2 && fruitData[0].length == 0) {
     for (key in allowedKeys) {
-        pokeData[pokemonNum - 1].push({
-            axis: allowedKeys[key],
-            value: data[id - 1][allowedKeys[key]]
-        })
+      fruitData[0].push({
+        axis: allowedKeys[key],
+        value: 0,
+      });
     }
+  }
+  // Formatiranje podataka kako bi se mogla koristiti RadarChart biblioteka
+  for (key in allowedKeys) {
+    fruitData[fruitNum - 1].push({
+      axis: allowedKeys[key],
+      value: data.find((element) => element.name == id.name)[allowedKeys[key]],
+    });
+  }
 
-    // Definiranje boja za radarski graf
-    var colorRadar = d3.scale.ordinal()
-        .range(["#EDC951", "#6FAD5E"]);
+  // Definiranje boja za radarski graf
+  var colorRadar = d3.scale.ordinal().range(["#9D090150", "#09099950"]);
 
-    // Definiranje opcija za radarski graf
-    var radarChartOptions = {
-        w: widthRadar - 100,
-        h: heightRadar - 100,
-        margin: marginRadar,
-        maxValue: 120,
-        levels: 6,
-        roundStrokes: false,
-        color: colorRadar
-    };
+  // Definiranje opcija za radarski graf
+  var radarChartOptions = {
+    w: widthRadar,
+    h: heightRadar,
+    margin: marginRadar,
+    maxValue: maxValue,
+    levels: 6,
+    roundStrokes: false,
+    color: colorRadar,
+  };
 
-    // Crtanje radarskog grafa i stupčastog grafa
-    // IZVORI:
-    // http://bl.ocks.org/nbremer/21746a9668ffdf6d8242
-    // https://gist.github.com/nbremer/21746a9668ffdf6d8242
-    // https://www.visualcinnamon.com/2015/10/different-look-d3-radar-chart.html
-    RadarChart(".radarChart", pokeData, radarChartOptions);
+  // Crtanje radarskog grafa i stupčastog grafa
+  // IZVORI:
+  // http://bl.ocks.org/nbremer/21746a9668ffdf6d8242
+  // https://gist.github.com/nbremer/21746a9668ffdf6d8242
+  // https://www.visualcinnamon.com/2015/10/different-look-d3-radar-chart.html
+  RadarChart(".radarChart", fruitData, radarChartOptions);
 }
 
-
-//Funkcija za pronalaženje ID-ja Pokemona u ovisnosti o predanom imenu
-function findPokemonByName(name, data) {
-    return data.find(obj => obj.Name == name)["#"];
+//Funkcija za pronalaženje ID-ja voća u ovisnosti o predanom imenu
+function findfruitByName(name, data) {
+  return data.find((obj) => obj.name == name);
 }
